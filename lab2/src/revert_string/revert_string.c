3501442fa9001a8044f21cd29ffa4703bc5467a2
Kraskov Alexandr <sanor00@yandex.ru>
2020-03-19 22:10:09 +0300

    add: lab2/answers; nice_echo; .gitignore

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
+	for(int i = 0; i < strlen(str) / 2; i++) {
+        char temp=str[i];
+        str[i]=str[strlen(str)-i-1];
+        str[strlen(str)-i-1]=temp;
+    }
 	// your code here
 }
 
